c39a5e6823499b7c35edbc95cda807203ceab878
Alvaro <alvgaona@gmail.com>
2020-04-19 12:24:23 -0300

    feat: Finish up exercise 9

--- linear_models/R/simulation.R
+++ linear_models/R/simulation.R
@@ -2,11 +2,52 @@ require(graphics)
 
 # Exercise 9
 
-## Histogram, boxplot and Q-Qnorm for a gaussian distribution
+## Histogram, boxplot and Q-Qnorm for a normal distribution
 x1 <- rnorm(1000)
 
-hist(x1)
-boxplot(x1)
+hist(x1, main = "Normal histogram")
+boxplot(x1, main = "Normal boxplot",
+        ylab = "Sample quantities",
+        xlab = "Groups",
+        col = rgb(0.1,0.1,0.9,0.1))
 qqnorm(x1); qqline(x1, col = 2)
 
-# TODO: Add plots for binomial, chi-square, F-distribution and gamma distributions
+## Histogram, boxplot and Q-Qnorm for a binomial distribution
+x2 <- rbinom(1000,10,0.4)
+
+hist(x2, main = "Binomial histogram")
+boxplot(x2, main = "Binomial boxplot",
+        ylab = "Sample qunatities",
+        xlab = "Groups",
+        col = rgb(0.7,0.3,0.4,0.9))
+qqnorm(x2); qqline(x2, col = 2)
+
+## Histogram, boxplot and Q-Qnorm for a chi-square distribution
+x3 <- rchisq(1000,50)
+
+hist(x3, main = "Chi-square histogram")
+boxplot(x3, main = "Chi-square boxplot",
+        ylab = "Sample qunatities",
+        xlab = "Groups",
+        col = rgb(0.5,0.2,0.4,0.6))
+qqnorm(x3); qqline(x3, col = 2)
+
+## Histogram, boxplot and Q-Qnorm for a F distribution
+x4 <- rf(1000,90,40)
+
+hist(x4, main = "F histogram")
+boxplot(x4, main = "F boxplot",
+        ylab = "Sample qunatities",
+        xlab = "Groups",
+        col = rgb(0.8,0.1,0.3,0.6))
+qqnorm(x4); qqline(x4, col = 2)
+
+## Histogram, boxplot and Q-Qnorm for a gamma distribution
+x5 <- rgamma(1000,0.7)
+
+hist(x5, main = "Gamma histogram")
+boxplot(x5, main = "Gamma boxplot",
+        ylab = "Sample qunatities",
+        xlab = "Groups",
+        col = rgb(0.1,0.1,0.7,0.5))
+qqnorm(x5); qqline(x5, col = 2)
