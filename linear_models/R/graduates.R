fdb4a8212b087545225793225cfa898aa52d4de6
Alvaro <alvgaona@gmail.com>
2020-04-19 21:57:18 -0300

    fix: Replace equals with back arrow

--- linear_models/R/graduates.R
+++ linear_models/R/graduates.R
@@ -2,16 +2,16 @@
 
 ## Read graduates grades subscribed in posgraduate program at the Department of Industrial
 ## Engineering in University of Berkeley.
-graduates = read.csv("./linear_models/data/graduates.csv")
-grades = graduates$Grade
+graduates <- read.csv("./linear_models/data/graduates.csv")
+grades <- graduates$Grade
 
-grades_mean = mean(grades)
-grades_median = median(grades)
-grades_trimmed_mean = mean(grades, trim = 0.1)
+grades_mean <- mean(grades)
+grades_median <- median(grades)
+grades_trimmed_mean <- mean(grades, trim = 0.1)
 
-grades_sd = sd(grades)
-grades_iqr = IQR(grades)
-grades_mad = mad(grades)
+grades_sd <- sd(grades)
+grades_iqr <- IQR(grades)
+grades_mad <- mad(grades)
 
 boxplot(grades, main = "Grades boxplot", xlab = "Graduates", ylab = "Grades")
 qqnorm(grades, main = "Grades | Normal Q-Q Plot"); qqline(grades) 
